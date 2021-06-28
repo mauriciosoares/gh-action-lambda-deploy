b1d08fb8edd30aec1826c713d46bdd67b19c5e69
Mauricio Soares <m.soares2603@gmail.com>
2021-06-28 17:44:35 -0300

    add typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import get from 'lodash/fp/get'
+
+export const handler = async (event) => {
+  const obj = {
+    message: 'Hello from Lambda! GH-Action, now with TS!'
+  }
+  const response = {
+      statusCode: 200,
+      body: get('message', obj),
+  };
+  return response;
+};
