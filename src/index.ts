0d5e0bbcb9416d438e0e2a5c383207e85bc4cbe2
Mauricio Soares <m.soares2603@gmail.com>
2021-06-28 17:54:31 -0300

    hello world

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import get from 'lodash/fp/get'
 
 export const handler = async (event) => {
   const obj = {
-    message: 'Hello from Lambda! GH-Action, now with TS!'
+    message: 'Hello from Lambda! GH-Action, now with TS! - Hello World'
   }
   const response = {
       statusCode: 200,
